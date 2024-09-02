c9b937d19850ebd70a6084423ba7b024613cba77
Pipo <Pipo2410@gmail.com>
2024-09-02 11:28:53 +0100

    fix layout for invoices page

--- app/invoices/page.tsx
+++ app/invoices/page.tsx
@@ -3,7 +3,7 @@ import { PageHeader } from '@/components/page/invoices/page-header';
 export default function Home() {
 	return (
 		<>
-			<main className="w-full">
+			<main className="w-full h-fit flex flex-col gap-10 ml-24">
 				<PageHeader />
 			</main>
 		</>
